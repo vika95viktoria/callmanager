ce263c67edb04dbd8fbe474efb0ba82c00d604d8
Viktoryia Khlystun <viktoryia_khlystun@epam.com>
2018-12-04 15:19:19 +0300

    added posibility to change folder

--- src/main/java/com/leveris/call/manager/service/impl/FileServiceImpl.java
+++ src/main/java/com/leveris/call/manager/service/impl/FileServiceImpl.java
@@ -11,23 +11,27 @@ import com.leveris.call.manager.service.FileService;
 import com.leveris.call.manager.service.FormatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@PropertySource("classpath:application.properties")
 public class FileServiceImpl implements FileService {
 
-    private static final String DIRECTORY = "D://callmanager/callinfo/";
+    private String directory;
 
     private FormatService formatService;
 
     @Autowired
-    public FileServiceImpl(FormatService formatService) {
+    public FileServiceImpl(FormatService formatService, @Value("${file.folder}") String directory) {
         this.formatService = formatService;
+        this.directory = directory;
     }
 
     public void writeCallToFile(String fileName, CallTO callTO) {
-        try(FileWriter fileWriter = new FileWriter(DIRECTORY + fileName + ".txt", true)) {
+        try(FileWriter fileWriter = new FileWriter(directory + fileName + ".txt", true)) {
             fileWriter.write(String.format("%s %10s \r\n",
                     formatService.formatPhone(callTO.getTelephone()),
                     new SimpleDateFormat("HH:mm:ss").format(new Date())));
